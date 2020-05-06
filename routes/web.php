8800f0edd675942248eebf37a7e14533e5b67d44
Muslehuddin Juned <MuslehuddinJuned@users.noreply.github.com>
2020-05-06 15:36:11 +0600

    update home page

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'QuestionsController@index');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'QuestionsController@index')->name('home');
 Route::resource('questions', 'QuestionsController')->except('show');
 Route::resource('questions.answers', 'AnswersController')->except(['index', 'create', 'show']);
 Route::get('/questions/{slug}', 'QuestionsController@show')->name('questions.show');
