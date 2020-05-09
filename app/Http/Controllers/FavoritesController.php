b73a2ef69151ab859f9682e84729108b469d4dfd
Muslehuddin Juned <MuslehuddinJuned@users.noreply.github.com>
2020-05-09 16:36:15 +0600

    Azaxify favorite button

--- app/Http/Controllers/FavoritesController.php
+++ app/Http/Controllers/FavoritesController.php
@@ -12,11 +12,19 @@ class FavoritesController extends Controller
     }
     public function store(Question $question){
         $question->favorites()->attach(auth()->id());
+
+        if(request()->expectsJson()){
+            return response ()->json(null, 204);
+        }
         return back();
     }
 
     public function destroy(Question $question){
         $question->favorites()->detach(auth()->id());
+
+        if(request()->expectsJson()){
+            return response ()->json(null, 204);
+        }
         return back();
     }
 }
--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -37,12 +37,13 @@
                                 @csrf
                                 <input type="hiden" name="vote" value="-1">
                             </form>
-                            <a title="Click to mark as favorite question (Click again to undo)" 
-                            class="favorite mt-2 {{Auth::guest() ? 'off' : ($question->is_favorited ? 'favorited' : '')}}"
-                            onclick="event.preventDefault(); document.getElementById('favorite-question-{{ $question->id }}').submit();">
-                                <i class="fas fa-star fa-2x"></i><br/>
-                                <span class="favorites-count">{{$question->favorites_count}}</span>
-                            </a>
+                            <favorite :question="{{ $question }}"></favorite>
+
+                            {{-- Favorite --}}
+
+
+
+
                             <form id="favorite-question-{{ $question->id }}" action="/questions/{{$question->id}}/favorites" method="POST" style="display:none;">
                                 @csrf
                                 @if ($question->is_favorited)
